d05d659e3dadd4717ad2ab1a2689b650d61ab269
kanha-beep <kanhashree2223@gmail.com>
2025-11-07 13:17:40 +0530

    msg

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const FRONTEND_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api"
+const FRONTEND_URL = import.meta.env.VITE_API_URL
 export const api = axios.create({
     baseURL: FRONTEND_URL,
     withCredentials: true
